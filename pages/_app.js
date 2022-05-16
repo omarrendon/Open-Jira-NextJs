ab6e0cf5507c203bba76870279dfaa644979f605
Omar Rendon <omarcr.96@gmail.com>
2022-05-16 13:00:47 -0500

    basic configuration of the project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
+import '../styles/globals.css';
+
+const basicTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={basicTheme}>
+      <CssBaseline/>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
