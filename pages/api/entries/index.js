b0ee3b0e96b9be2cad7342e59d806780cdf2dc2d
Omar Rendon <omarcr.96@gmail.com>
2022-06-09 16:03:23 -0500

    ADD: request to entries data into data base

---
+++ pages/api/entries/index.js
@@ -0,0 +1,22 @@
+import { db } from "../../../database";
+import { Entry } from "../../../models";
+
+export default function handler (req, res) {
+
+  switch (req.method) {
+    case 'GET':
+      return getEntries(res);
+    default:
+      return res.status(400).json({ massage: 'Entries not found' });
+  }
+}
+
+const getEntries = async (res, req) => {
+  await db.connect();
+  const entries = Entry.find().sort({ createdAt: 'ascending'});
+
+  await db.disconnect();
+  res.status(200).json({
+    entries
+  });
+};
