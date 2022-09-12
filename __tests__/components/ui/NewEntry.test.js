04639581bb11c954545080529a6034dbed252d2e
Omar Rendon <omarcr.96@gmail.com>
2022-09-12 12:23:33 -0500

    Add: NewEntry new tests

--- __tests__/components/ui/NewEntry.test.js
+++ __tests__/components/ui/NewEntry.test.js
@@ -1,15 +1,17 @@
-import { NewEntry } from "../../../components/ui";
-import { render, screen, fireEvent } from "@testing-library/react";
 import '@testing-library/jest-dom';
+import { fireEvent, render, screen } from "@testing-library/react";
+import { NewEntry } from "../../../components/ui";
 
 
 describe('<NewEntry /> Component', () => {
   let buttonAddEntry;
+  let cancelButton;
+  let saveButton;
+  let textField;
   // Before each test we mount NewEntry Component and get it's elements.
   beforeEach(() => {
     render(<NewEntry />);
     buttonAddEntry = screen.getByRole('button');
-
   });
 
   test('should be able the button to add new task', () => {
@@ -21,4 +23,23 @@ describe('<NewEntry /> Component', () => {
     const textField = screen.getByTestId('new-entry-input');
     expect(textField).toBeInTheDocument();
   });
+
+  test('should be show cancelar and guardar button when press the button "Agregar Tarea"', () => {
+    fireEvent.click(buttonAddEntry);
+
+    cancelButton = screen.getByRole('button', { name: 'Cancelar' });
+    saveButton = screen.getByRole('button', { name: 'Guardar' });
+
+    expect(cancelButton).toBeInTheDocument();
+    expect(saveButton).toBeInTheDocument();
+  });
+
+  test('should be contain the payload and reset the states after save a new entry', () => {
+    textField = screen.getByLabelText('Nueva entrada');
+  });
+
+  // test('should be disappear the textfield and guardar and cancelar buttons after click in cancelar button', () => {
+  //   fireEvent.click(cancelButton);
+  //   expect(buttonAddEntry).toBeInTheDocument();
+  // });
 });
